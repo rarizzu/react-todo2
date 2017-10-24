ebf18421be203106dde882180b3291f213cb9637
Richard Rizzuto <richardrizzuto@richards-mbp-2.attlocal.net>
2017-10-23 20:38:04 -0500

    add static todoList

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var TodoApp = require('TodoApp');
 
 
 
@@ -15,6 +16,6 @@ require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
-        <p>Boilerplate React</p>,
+        <TodoApp />,
         document.getElementById('app')
     );
--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,16 @@ module.exports = {
     },
     resolve: {
         root: __dirname,
+        modulesDirectories: [
+            'node_modules',
+            './app/components'
+        ],
         alias: {
             applicationStyles: 'app/styles/app.scss'
         },
         extensions: ["",".js",".jsx"]
     },
+    
     module: {
         loaders: [
             {
